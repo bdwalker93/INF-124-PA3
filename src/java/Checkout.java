042747526d4e44ec659ba27eee6d462b1b6f005d
amamdani15 <amamdani15@gmail.com>
2016-06-01 02:36:52 -0700

    Products Page table done
    finished up the table. need to set it up as a loop. i don’t know if the
    cart is set up fully. just need to loop thru that to show everything.
    
    also need to add the form at the bottom for the user to fill out. not
    sure how that information is being sent to the database, but will need
    to set that up too.

--- src/java/Checkout.java
+++ src/java/Checkout.java
@@ -34,12 +34,84 @@ public class Checkout extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Checkout</title>");            
+            
+            out.println("<title>Products</title>");            
+        out.println("<meta charset=\"UTF-8\">");            
+        out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");            
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style_sheets/checkout_2_style.css\">");
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style_sheets/navigation_style.css\">");
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style_sheets/body_style.css\">");
+                
+        out.println("</head>");
+        out.println("<body>");
+
+        /*BEGINNING OF PAGE*/
+        //nav bar
+        out.println(" <!--        This is the navigator-->\n" +
+            "        <nav>\n" +
+            "         <ul>\n" +
+            "            <li><a href=\"index.html\">Home</a></li>\n" +
+            "            <li><a href=\"Products\">Products</a></li>\n" +
+            "            <li><a href=\"meet_the_team.html\">Meet The Team</a></li>\n" +
+            "            <li style=\"\"><a  href=\"about.html\">About Us</a></li>\n" +
+            "`           <li style =\"float:right; background-color:red\"><a class=\"theme_color\" href=\"Checkout\">Checkout</a></li>\n" +
+            "        </ul>\n" +
+            "        </nav>");
+            
+            
+            out.println("<title>Checkout</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Checkout at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Checkout" + "</h1>");
+            
+            out.println("<h2 style=\"text-align:center\">Items in cart:" + "</h2>");
+            
+            out.println("<table class = \"outside_table\">");
+            out.println("<tr class = \"tr1\">");
+            out.println("<td class = \"td0\">");
+//            out.println("Hello! World");
+            out.println("</td>");
+            out.println("<td class = \"td1\">");
+            out.println("Product");
+            out.println("</td>");
+            out.println("<td class = \"td2\">");
+            out.println("Qty");
+            out.println("</td>");
+            out.println("<td class = \"td3\">");
+            out.println("Total");
+            out.println("</td>");
+            out.println("</tr>");
+            
+            
+            out.println("<tr class = \"tr1\">");
+            out.println("<td class = \"td0\">");
+            out.println("<img style = \"max-width:100px; max-height: 100px;\" src=\"/PA3/images/product_images/AP1.jpg\"");
+            out.println("</td>");
+            out.println("<td class = \"td1\">");
+            out.println("Audimar Pidet AP1");
+            out.println("</td>");
+            out.println("<td class = \"td2\">");
+            out.println("2");
+            out.println("</td>");
+            out.println("<td class = \"td3\">");
+            out.println("$38.50");
+            out.println("</td>");
+            out.println("</tr>");
+            
+            
+            
+            out.println("</table>");
+            
+            
             out.println("</body>");
             out.println("</html>");
+            
+            
+            
+            
+            
+            
+            
         }
     }
 
